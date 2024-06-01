d99d394b0f23ce5a88b1f9554dfb59d208b17d1e
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2024-06-02 01:58:04 +0200

    enabled generateLocaleConfig
    Change-Id: I0bef0382d795651611a6a1ba5f655f80470a6c9c
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hifnawy.caffeinate"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.2.5"
+        versionCode = 6
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -54,6 +54,11 @@ android {
 
     buildFeatures {
         viewBinding = true
+    }
+
+    androidResources {
+        @Suppress("UnstableApiUsage")
+        generateLocaleConfig = true
     }
 }
 
