fa2f8f66932f2a50fe0d4d5d55d6f8c8bdcaac8b
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2024-06-02 14:11:51 +0200

    bump app version to 1.2.7
    Change-Id: I750cd65435f15f9d3f488df52416d5bdddf7aa20
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hifnawy.caffeinate"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.2.6"
+        versionCode = 7
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
