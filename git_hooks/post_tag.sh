c0c8707fdaeddb2d9276bfe0baf4a9647d2ba349
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2024-06-03 17:38:59 +0200

    add post_tag.sh hook
    since there are no git hooks for pre/post tag, I created an alias called rtag to call post_tag.sh
    
    Change-Id: I8ecdc618606f5470d8e1f4dc25317ce3bd51522a
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

---
+++ git_hooks/post_tag.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# add:
+# [alias]
+#   rtag = !git_hooks/post_tag.sh
+# to .git/config or from git config --edit
+# to alias this script to 'rtag'
+
+if [ "$1" ]; then
+   if git tag $1 &>/dev/null; then
+      gitTopLevel=$(git rev-parse --show-toplevel)
+
+      if python "$gitTopLevel/git_hooks/update_publish_tags.py" "$gitTopLevel"; then
+         echo "adding '$gitTopLevel/.github/workflows/publish_release.yml' to git staging..."
+
+         git add "$gitTopLevel/.github/workflows/publish_release.yml"
+
+         git commit --amend --no-edit
+      fi;
+   else
+      echo fatal: tag "'$1'" already exists
+   fi;
+
+   exit 93
+else
+   git tag
+fi;
