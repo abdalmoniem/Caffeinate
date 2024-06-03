18982d8e10e34797a5027db4f78ca6ec3175a6cc
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2024-06-03 17:04:06 +0200

    bump app version to 1.3.2
    changed git hooks from pre-commit to pre-push
    
    Change-Id: I256508711fd6cc849cd724a4c5fecf6126e62c62
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hifnawy.caffeinate"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.3.1"
+        versionCode = 11
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
