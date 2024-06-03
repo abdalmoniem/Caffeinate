18338d7fa26b1978691bc63b54df5366d4ca91ab
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2024-06-03 16:06:22 +0200

    bump app version to 1.3.1
    Change-Id: Ic2f4b129e02ad1f21f3838f9dac8c34208aee0b6
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hifnawy.caffeinate"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.3"
+        versionCode = 10
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
