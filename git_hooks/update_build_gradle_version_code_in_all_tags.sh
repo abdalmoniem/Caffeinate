195f60a2097b2cbfd5771a7b094bd6a2273ad76a
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2024-06-07 22:15:57 +0200

    updated update_build_gradle_version_code_in_all_tags.sh
    Change-Id: I31faf240337ee7766b068c2c428296964cad6942
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

--- git_hooks/update_build_gradle_version_code_in_all_tags.sh
+++ git_hooks/update_build_gradle_version_code_in_all_tags.sh
@@ -2,47 +2,45 @@ startingVersion=1
 
 HEAD=$(git rev-parse HEAD)
 currentBranch=$(git rev-parse --abbrev-ref HEAD)
+# shellcheck disable=SC2207
 tags=($(git tag))
 
 filename=./app/build.gradle.kts
 filter="\(versionCode\s\+=\s\+\)\([[:digit:]]\+\)"
 
-sed -i "s/$filter/\1$startingVersion/" $filename
-git add $filename
-git commit --amend --no-edit --quiet # || continue
-
 echo "HEAD is at $currentBranch -> $HEAD"
 
 # Iterate over all tags
-for tag in ${tags[@]}
+for tag in "${tags[@]}"
 do
   echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
   echo "checking out tag: $tag ..."
+
   # Check out the tag
   git checkout --quiet $tag
+
+  commitMessage=$(git show $tag -s --format='%s%n%n%b' | sed -e 's/Change-Id:\s*\w\+//' | sed -e 's/Signed-off-by:\s*.*//')
+
+  echo "commit message: $commitMessage"
 
   echo "deleting tag $tag ..."
   git tag -d $tag
 
-  # # Replace the line in the file
+  # Replace the line in the file
   sed -i "s/$filter/\1$startingVersion/" $filename
   # cat $filename | sed -e "s/$filter/\1$startingVersion/" | grep -e $filter | tr -s ' \n'
 
   ((startingVersion+=1))
-
-#  echo "deleting tag $tag from origin..."
-#  git push origin :$tag # || continue
 
   # Commit the changes
   echo "adding $filename to git staging..."
   git add $filename
 
   echo "committing change: $(cat $filename | grep -e $filter | tr -s ' \n') ..."
-#  git commit --amend --no-edit --quiet # || continue
   git commit -sm "updated tag $tag" -m "updated $filename::versionCode=$startingVersion in tag $tag"
 
   echo "adding tag $tag ..."
-  git rtag $tag
+  git tag $tag
 
   echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
   echo
