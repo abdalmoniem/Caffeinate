3afb7431b101a8d1b187e1467e65eb3cd82e5481
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2024-06-08 08:35:30 +0200

    addde scripts to publish/draft all GitHub releases
    Change-Id: I3e6bbdcee4305bf20a9cfbee9791401c297cb687
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

---
+++ git_hooks/draft_github_releases.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Replace with your repository owner
+OWNER="abdalmoniem"
+# Replace with your repository name
+REPO="Caffeinate"
+
+# List all published releases and store their tags in a temporary variable
+published_tags=$(gh release list --repo "$OWNER/$REPO" --json tagName,isDraft | jq -r '.[] | select(.isDraft == false) | .tagName')
+
+# Reverse the order of release tags
+reversed_tags=$(echo "$published_tags" | tac)
+
+# Iterate over each published release tag
+for release in $reversed_tags
+do
+  release=$(echo $release | xargs)
+
+  # Update the release to be a draft again
+  gh release edit "$release" --repo "$OWNER/$REPO" --draft=true
+
+  echo "Redrafted release: $release"
+  echo
+
+  # Introduce a delay of 1 second to avoid rate limiting (optional)
+  # sleep 1
+done
+
+echo
+echo "All published releases redrafted."
---
+++ git_hooks/publish_github_draft_releases.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Replace with your repository owner
+OWNER="abdalmoniem"
+# Replace with your repository name
+REPO="Caffeinate"
+
+# List all draft releases and store their tags in a temporary variable
+draft_tags=$(gh release list --repo "$OWNER/$REPO" --json tagName,isDraft | jq -r '.[] | select(.isDraft == true) | .tagName')
+
+# Reverse the order of release tags
+reversed_tags=$(echo "$draft_tags" | tac)
+
+# Iterate over each draft release tag
+for release in $reversed_tags
+do
+  release=$(echo $release | xargs)
+
+  # echo "gh release edit "$release" --repo "$OWNER/$REPO" --draft=false"
+
+  # Publish the draft release
+  gh release edit "$release" --repo "$OWNER/$REPO" --draft=false --latest=true
+
+  echo "Published release: $release"
+  echo
+
+  # Introduce a delay of 1 second to avoid rate limiting (optional)
+  # sleep 1
+done
+
+echo
+echo "All draft releases published."
