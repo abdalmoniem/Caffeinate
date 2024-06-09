1879f46e66762c12e7b895beb5dd8d1c3e3f75a4
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2024-06-09 05:01:26 +0200

    fix post_tag.sh logic
    first of all, the script exited right after checking the versionCode and versionName. second, the check for existing tag was inverted
    
    Change-Id: Ie9c6b2ed8c29faf47cfc4560d928bcdeec6718ae
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

--- git_hooks/post_tag.sh
+++ git_hooks/post_tag.sh
@@ -3,7 +3,10 @@
 if [ "$1" ]; then
   newTag=$1
 
-  if git tag -l "$newTag" &>/dev/null; then
+  if [ -n "$(git tag -l "$newTag")" ]; then
+    echo "fatal: tag ""$newTag"" already exists"
+    exit 93
+  else
     buildGradleFile="app/build.gradle.kts"
 
     # shellcheck disable=SC2207
@@ -22,20 +25,38 @@ if [ "$1" ]; then
     currentVersionName=$(grep -m 1 versionName < $buildGradleFile | sed -e "s/$versionNameFilter/\2/" | xargs)
     currentVersionNameLineNumber=$(grep -nm 1 versionName < $buildGradleFile | grep -oe '[[:digit:]]\+:' | sed -e 's/\([[:digit:]]\+\):/\1/' | xargs)
 
+    versionCodeMatch=false
+    versionNameMatch=false
+
     if [ "$currentVersionCode" -le "$prevTagVersionCode" ]; then
-      echo "ERROR: current tag: $newTag has versionCode less than or equal to the previous tag: $prevTag, please change versionCode in "$buildGradleFile:$currentVersionCodeLineNumber""
+      echo "ERROR: current tag: $newTag has versionCode less than or equal to the previous tag: $prevTag, please change versionCode in "$buildGradleFile:"$currentVersionCodeLineNumber"""
       echo "$prevTag:$buildGradleFile:$prevTagVersionCodeLineNumber:versionCode = $prevTagVersionCode"
       echo "$newTag:$buildGradleFile:$currentVersionCodeLineNumber:versionCode = $currentVersionCode"
-      echo
+      versionCodeMatch=true
     fi
 
     if [ "$prevTagVersionName" == "$currentVersionName" ]; then
-      echo "ERROR: current tag: $newTag has versionName the same as the previous tag: $prevTag, please change versionName in "$buildGradleFile:$currentVersionNameLineNumber""
+      if [ $versionCodeMatch == true ]; then
+        echo
+      fi
+      echo "ERROR: current tag: $newTag has versionName the same as the previous tag: $prevTag, please change versionName in "$buildGradleFile:"$currentVersionNameLineNumber"""
       echo "$prevTag:$buildGradleFile:$prevTagVersionNameLineNumber:versionName = $prevTagVersionName"
       echo "$newTag:$buildGradleFile:$currentVersionNameLineNumber:versionName = $currentVersionName"
+      versionNameMatch=true
     fi
 
-    exit 93
+    if [ "$newTag" != "v$currentVersionName" ]; then
+      echo "tag $newTag doesn't match versionName $currentVersionName"
+    fi
+
+    if [ $versionCodeMatch == true ] || [ $versionNameMatch == true ]; then
+      echo
+      echo "ERROR: fix errors to add tag $newTag"
+      exit 23
+    else
+      echo "adding tag $newTag"
+      git tag "$newTag"
+    fi
 
     gitTopLevel=$(git rev-parse --show-toplevel)
 
@@ -43,14 +64,11 @@ if [ "$1" ]; then
       echo "adding '$gitTopLevel/.github/workflows/publish_release.yml' to git staging..."
 
       git add "$gitTopLevel/.github/workflows/publish_release.yml"
+      git add "$buildGradleFile"
 
       git commit --amend --no-edit
     fi;
-  else
-    echo fatal: tag "'$newTag'" already exists
   fi;
-
-  exit 93
 else
   git tag
 fi;
